8cff2a74312bf5f608701eff4a6e671ee4c847bc
Rohan Mehta <rmehta98@gatech.edu>
2022-01-30 00:37:53 -0500

    general format of homepage complete

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='appHeader'>
+        <h3><strong>Budget-Mate</strong></h3>
       </header>
+      <div className='peoplePane'>
+        <h5 className='paneHeader'>People</h5>
+      </div>
+      <div className='itemPane'>
+        <h5 className='paneHeader'>Items</h5>
+      </div>
     </div>
   );
 }
