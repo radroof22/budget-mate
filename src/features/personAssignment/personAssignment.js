33790650b097b10aea8b5e7d9b02de07b979fde2
Rohan Mehta <rmehta98@gatech.edu>
2022-02-26 18:59:09 -0500

    refractor checking if participatingPerson selector

--- src/features/personAssignment/personAssignment.js
+++ src/features/personAssignment/personAssignment.js
@@ -4,17 +4,27 @@ import "./PersonAssignment.css"
 
 function PersonAssignment({itemId}) {
     // Setup redux
-    const people = useSelector(state => state.people.people)
-    const assignments = useSelector(state => state.personAssignments.assignments)
+    const people = useSelector(state => state.people.people.map(person => {
+        let newPerson = {
+            id: person.id,
+            name: person.name,
+            assignedItem: false
+        }
+        
+        newPerson.assignedItem = state.personAssignments.assignments.filter(assignment => 
+            { 
+                return assignment.itemId === itemId && assignment.personId === person.id 
+            }).length > 0
+        return newPerson;
+    }))
     const dispatch = useDispatch()
 
-    function handlePersonClick(personId) {
+    function handlePersonClick(person) {
         let payload = {
-            "personId": personId,
+            "personId": person.id,
             "itemId": itemId,
         }
-        if (personParticipating(personId)) {
-            console.log("lets remove")
+        if (person.assignedItem) {
             dispatch(removeAssignment(payload))
         } else {
             dispatch(assignPerson(payload))
@@ -22,19 +32,14 @@ function PersonAssignment({itemId}) {
         
     }
 
-    function personParticipating(personId) {
-        return assignments.filter(a => {
-            return a.personId === personId && a.itemId === itemId
-        }).length > 0
-    }
     return (
         <div>
             <ul className="addPeople">
               {people.map(person => {
                   return (
-                    <li onClick={ () => handlePersonClick(person.id) } 
+                    <li onClick={ () => handlePersonClick(person) } 
                         key={person.id}
-                        className = {personParticipating(person.id) ? "included" : null}>
+                        className = {person.assignedItem ? "included" : null}>
                             {person.name}
                     </li>
                   )
